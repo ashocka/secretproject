fd1700dc65a2dcd571301a62e760228ce233a571
Heidi Pungartnik <heidi@ashocka.com>
2018-04-03 18:00:25 +0200

    level-2 funcationality done

--- js/level-2.js
+++ js/level-2.js
@@ -1,4 +1,31 @@
 jQuery( document ).ready(function() {
 
+	var i = 1;
+
+	$('.cat').click(function(){
+		if (i === 10) {
+
+			$(this).animate({
+			    left: "900px",
+			  }, 2000, function() {});
+
+		} // end if
+
+		i++;
+	});
+
+	$('.access-card').click(function(){
+			$('.access-granted').show();
+			
+			setTimeout(function(){ // create a global function from this
+				$('.fade-in').show().animate({
+			    	opacity: 1,
+			  	}, 1000, function() {});
+
+				setTimeout(function(){
+					window.location.href = "level-3.html";
+				}, 1000);
+			}, 1000);
+		});
 
 });
